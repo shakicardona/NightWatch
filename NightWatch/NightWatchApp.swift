ff4ba15f1a24a8ca42a7273a3c159ec95dbba775
shakicardona <mar.cardona@outlook.es>
2025-12-01 17:58:41 +0100

    Initial Commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Mar Cardona Aranda on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
