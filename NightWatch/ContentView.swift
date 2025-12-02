04e27caae98c19cc9edac6891bad63924f3d3864
shakicardona <mar.cardona@outlook.es>
2025-12-02 13:03:16 +0100

    ui appearance with view modifiers

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -9,16 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            VStack (alignment: .leading){
+                Text("\(Image(systemName: "moon.stars")) Nightly Tasks")
+                    .modifier(HeaderStyle())
+                Text("Check all windows")
+                Text("Check all doors")
+                Text("Check that the safe is locked")
+                Text("Inspect security cameras")
+                Text("Clear ice from sidewalks")
+                Text("Documents \"strange and unusual\" occurrences")
+                
+                Text("\(Image(systemName: "sunset")) Weekly Tasks")
+                    .headerStyle()
+                    .padding(.top)
+                Text("Check inside all vacant rooms")
+                Text("Walk the perimeter of property")
+                
+                Text(" \(Image(systemName: "calendar"))Monthly Tasks")
+                    .headerStyle()
+                    .padding(.top)
+                Text("Test security alarm")
+                Text("Test motion detectors")
+                Text("Test smoke alarms")
+                Spacer()
+            }
+            .foregroundStyle(.gray)
+            Spacer()
         }
-        .padding()
+        .padding(.all, 10.0)
+    }
+}
+
+struct HeaderStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title3)
+            .foregroundStyle(.yellow)
+            .textCase(.uppercase)
+            .underline()
+    }
+}
+
+extension View {
+    func headerStyle() -> some View {
+        self.modifier(HeaderStyle())
     }
 }
 
 #Preview {
     ContentView()
+}
+
+#Preview("ContentView Landscape", traits:
+        .landscapeRight,
+         body: { ContentView()
+})
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
